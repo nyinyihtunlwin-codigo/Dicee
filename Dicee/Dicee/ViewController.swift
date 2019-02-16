7684901880ae76401c945534a31c14289978fcfb
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2019-02-16 10:13:39 +0630

    Project skeleton created.

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Administrator on 2/16/19.
+//  Copyright © 2019 Nyi Nyi Htun Lwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
